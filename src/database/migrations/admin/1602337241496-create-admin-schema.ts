b110f1bf1b2a13ced74a15be1584882fd2f7aaa5
Phaney Phin <phenpani@gmail.com>
2025-01-01 13:44:14 +0700

    Add create extension migration

--- src/database/migrations/admin/1602337241496-create-admin-schema.ts
+++ src/database/migrations/admin/1602337241496-create-admin-schema.ts
@@ -4,6 +4,7 @@ const schemaName = 'admin';
 export class CreateAdminSchema_1602337241496 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createSchema(schemaName, true);
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
