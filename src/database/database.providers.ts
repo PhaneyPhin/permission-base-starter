de0d2a80b789377bf79191bcf3adbda1970fd1c9
Phaney Phin <phenpani@gmail.com>
2025-02-27 10:15:46 +0700

    Fix id increase

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,26 +1,25 @@
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { join } from 'path';
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule, TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 /**
  * Setup default connection in the application
  * @param config {ConfigService}
  */
 ConfigModule.forRoot({
-  envFilePath: '.env',
+  envFilePath: ".env",
 });
 
 const defaultConnection = (config: ConfigService): TypeOrmModuleOptions => ({
-  type: 'postgres',
-  host: config.get('TYPEORM_HOST'),
-  port: config.get<number>('TYPEORM_PORT'),
-  username: config.get('TYPEORM_USERNAME'),
-  password: config.get('TYPEORM_PASSWORD'),
-  database: config.get('TYPEORM_DATABASE'),
+  type: "postgres",
+  host: config.get("TYPEORM_HOST"),
+  port: config.get<number>("TYPEORM_PORT"),
+  username: config.get("TYPEORM_USERNAME"),
+  password: config.get("TYPEORM_PASSWORD"),
+  database: config.get("TYPEORM_DATABASE"),
   autoLoadEntities: false, // Disable autoLoadEntities for glob pattern
-  entities: [__dirname + '/../**/*.entity.js'] ,
-  synchronize: config.get<boolean>('TYPEORM_SYNCHRONIZE') === true,
-  logging: config.get<boolean>('TYPEORM_LOGGING') === true,
+  entities: [__dirname + "/../**/*.entity.js"],
+  synchronize: config.get<boolean>("TYPEORM_SYNCHRONIZE") === true,
+  logging: config.get("TYPEORM_LOGGING") === "true",
 });
 
 export const databaseProviders = [
